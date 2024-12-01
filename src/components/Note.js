d9970d23f0624828be87d2f013cf1ea4dfb33b43
ggorcl <garvit.oracle.05@gmail.com>
2024-12-01 21:22:45 +0530

    note initialize

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,8 @@
 export function Note(){
     return (
-        <div>
-        NOTE
+        <div className="note">
+            <h3 className="note-heading">{new Date().toUTCString().toString()}</h3>
+            <textarea className="note-content"></textarea>
         </div>
     )
 }
