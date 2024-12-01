3656c0a1f61068c3c44d1bcaf7060991b698a87e
ggorcl <garvit.oracle.05@gmail.com>
2024-12-01 20:47:02 +0530

    show/hide all folders section

--- src/App.js
+++ src/App.js
@@ -9,23 +9,28 @@ function App() {
   const [allFoldersVisible, setAllFoldersVisible] = useState(true);
 
   const toggleAllFoldersVisiblity = () => {
-    setAllFoldersVisible(prev => !prev)
-  }
+    setAllFoldersVisible((prev) => !prev);
+  };
 
   return (
     <div className="app">
       <div id="top-bar">
         <MacButtons />
-        <ToggleButton primaryColor="#E49273" toggleAllFoldersVisiblity={toggleAllFoldersVisiblity}/>
+        <ToggleButton
+          primaryColor="#E49273"
+          toggleAllFoldersVisiblity={toggleAllFoldersVisiblity}
+        />
       </div>
-      <div className="folders-container section" style={allFoldersVisible ? {width: "100%"} : {width: "0%"}}>
-        <Folders/>
+      <div
+        className="folders-container section"
+      >
+        <Folders />
       </div>
-      <div className="folder-container section">
-        <Folder/>
+      <div className="folder-container section" style={allFoldersVisible ? {width: "25%", left: "25%"} : {width: "30%", left: "0"}}>
+        <Folder />
       </div>
-      <div className="note-container section">
-        <Note/>
+      <div className="note-container section" style={allFoldersVisible ? {width: "50%"} : {width: "70%"}}>
+        <Note />
       </div>
     </div>
   );
