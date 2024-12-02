3c0032eaa124c892ac9e372f7a9a3c72466823ff
ggorcl <garvit.oracle.05@gmail.com>
2024-12-02 17:51:34 +0530

    maximize / minimize

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MacButtons } from "./components/MacButtons";
 import { ToggleButton } from "./components/ToggleButton";
 import { Folders } from "./components/Folders";
@@ -13,16 +13,41 @@ function App() {
   const primaryColor = useSelector((state: RootState) => state.primaryColor)
   const selectedFolder = useSelector((state: RootState) => state.selectedFolder)
   const [allFoldersVisible, setAllFoldersVisible] = useState(true);
+  const [isFullScreen, setIsFullScreen] = useState(false);
   const dispatch = useDispatch()
 
   const toggleAllFoldersVisiblity = () => {
     setAllFoldersVisible((prev) => !prev);
   };
 
+  function maximizeScreen() {
+    setIsFullScreen(true);
+  }
+
+  function minimizeScreen() {
+    setIsFullScreen(false);
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const isInputFocused = ['INPUT', 'TEXTAREA'].includes(document.activeElement?.tagName || '');
+
+      if (!isInputFocused && event.key === 'f') {
+        setIsFullScreen(!isFullScreen)
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isFullScreen]);
+
   return (
-    <div className="app" style={{ "--primary-color": `${primaryColor}` } as React.CSSProperties}>
+    <div className="app" style={{ "--primary-color": `${primaryColor}`, height: isFullScreen ? "100%" : "70%", width: isFullScreen ? "100%" : "80%" } as React.CSSProperties}>
       <div id="top-bar">
-        <MacButtons />
+        <MacButtons maximize={maximizeScreen} minimize={minimizeScreen} />
         <ToggleButton
           toggleAllFoldersVisiblity={toggleAllFoldersVisiblity}
         />
--- src/components/MacButtons.tsx
+++ src/components/MacButtons.tsx
@@ -1,11 +1,11 @@
-export function MacButtons(){
+export function MacButtons({ maximize, minimize }: { maximize: () => void, minimize: () => void }) {
     return (
         <>
-        <div className="mac-buttons">
-            <div className="mac-button mac-close-button"></div>
-            <div className="mac-button mac-minimize-button"></div>
-            <div className="mac-button mac-maximize-button"></div>
-        </div>
+            <div className="mac-buttons">
+                <div className="mac-button mac-close-button"></div>
+                <div className="mac-button mac-minimize-button" onClick={() => minimize()}></div>
+                <div className="mac-button mac-maximize-button" onClick={() => maximize()}></div>
+            </div>
         </>
     )
 }
